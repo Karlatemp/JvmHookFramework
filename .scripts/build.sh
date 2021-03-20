dd748acd28a6ccb966807c8cbab884a280bd8660
Karlatemp <karlatemp@vip.qq.com>
2021-03-20 12:57:48 +0800

    Setup Workflow

---
+++ .scripts/build.sh
@@ -0,0 +1,12 @@
+cd native
+mkdir cmake-build-debug
+cd cmake-build-debug
+rm -rf CMakeCache.txt
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CACHEFILE_DIR=$PWD -Dcross_triple=$CROSS ..
+cmake --build . --target native -- -j 3
+
+mkdir bin
+
+cp *.dll bin
+cp *.so bin
+
---
+++ .scripts/setup_docker.sh
@@ -0,0 +1,3 @@
+
+docker run --rm -v $GITHUB_WORKSPACE:/work --env CROSS=$CROSS dockcross/$CROSS bash -c '/work/cross/build.sh'
+

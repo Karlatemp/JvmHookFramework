3ddbd4df16a095e3f7f352b837db597c6aacf37b
Karlatemp <karlatemp@vip.qq.com>
2021-03-20 13:04:09 +0800

    Setup Workflow

--- .scripts/setup_docker.sh
+++ .scripts/setup_docker.sh
@@ -1,3 +1,3 @@
 
-docker run --rm -v $GITHUB_WORKSPACE:/work --env CROSS=$CROSS dockcross/$CROSS bash -c '/work/cross/build.sh'
+docker run --rm -v $GITHUB_WORKSPACE:/work --env CROSS=$CROSS dockcross/$CROSS bash -c '/work/.scripts/build.sh'
 
